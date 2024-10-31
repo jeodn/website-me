05142468478e679085ca368e3fade393552a6553
jeodn <joshdrvillas@gmail.com>
2024-10-31 00:02:55 -0400

    website version 2 anas

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# website-me
-about me
+# portfolio
--- josh-splashtest.js
+++
@@ -1,8 +0,0 @@
-// JavaScript Document
-const splash = document.querySelector('.splash');
-
-document.addEventListener('DOMContentLoaded', (e)=>{
-	setTimeout(()=>{
-		splash.classList.add('display-none');
-	}, 2000);
-})
